73f472a8e496e48fc430c38c22d2d130a36180bd
Metallifax <61431322+Metallifax@users.noreply.github.com>
2021-01-02 14:44:45 -0400

    added a label

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 def window():
+  # Config
   app = QApplication(sys.argv)
   win = QMainWindow()
-  win.setGeometry(0, 0, 300, 300)
+  win.setGeometry(800, 400, 300, 300)
   win.setWindowTitle('Tech With Tim')
 
+  # Labels
+  label = QtWidgets.QLabel(win)
+  label.setText('My first label')
+  label.move(50, 50)
+
+  # Start
   win.show()
   sys.exit(app.exec_())
 
