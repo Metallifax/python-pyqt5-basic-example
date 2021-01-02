03211c79b89123d7c15abd99fa80fd342f85ea11
Metallifax <61431322+Metallifax@users.noreply.github.com>
2021-01-02 14:59:35 -0400

    added the MyWindow class for state management

--- main.py
+++ main.py
@@ -2,26 +2,40 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-def my_clicked():
-  print('clicked')
+class MyWindow(QMainWindow):
+  def __init__(self):
+    super(MyWindow, self).__init__()
+    self.setGeometry(800, 400, 300, 300)
+    self.setWindowTitle('Tech With Tim')
+    self.initUI()
+
+  def initUI(self):
+    # Labels
+    self.label = QtWidgets.QLabel(self)
+    self.label.setText('My first label')
+    self.label.move(50, 50)
+
+      # Buttons
+    self.b1 = QtWidgets.QPushButton(self)
+    self.b1.setText('Click me!')
+    self.b1.move(50, 75)
+    self.b1.clicked.connect(self.clicked)
+
+  def clicked(self):
+    if self.label.text() == 'You pressed the button':
+      self.label.setText("You pressed the button again!")
+      self.update()
+    else:
+      self.label.setText("You pressed the button")
+      self.update()
+
+  def update(self):
+    self.label.adjustSize()
 
 def window():
   # Config
   app = QApplication(sys.argv)
-  win = QMainWindow()
-  win.setGeometry(800, 400, 300, 300)
-  win.setWindowTitle('Tech With Tim')
-
-  # Labels
-  label = QtWidgets.QLabel(win)
-  label.setText('My first label')
-  label.move(50, 50)
-
-  # Buttons
-  b1 = QtWidgets.QPushButton(win)
-  b1.setText('Click me!')
-  b1.move(50, 75)
-  b1.clicked.connect(my_clicked)
+  win = MyWindow()
 
   # Start
   win.show()
